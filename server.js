b02128348c2acfc21e6a8bc212fabacc80d9dc62
Wang Yixuan <YixuanEric>
2026-03-04 00:14:08 +0800

    modify server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import crypto from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
